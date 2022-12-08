69f2f22328af2bc6f759ae0c01407bc67103959d
sarievo <sarievo@protonmail.com>
2022-12-08 18:19:21 +0800

    Restructured the location of definition

--- template/macro.hpp
+++ template/macro.hpp
@@ -1,11 +1,3 @@
-#define I(...) int __VA_ARGS__;in(__VA_ARGS__)
-#define L(...) long long __VA_ARGS__;in(__VA_ARGS__)
-#define S(...) string __VA_ARGS__;in(__VA_ARGS__)
-#define C(...) char __VA_ARGS__;in(__VA_ARGS__)
-#define D(...) double __VA_ARGS__;in(__VA_ARGS__)
-#define V(T, name, n) vector<T> name(n); in(name)
-#define VV(T, name, n, m) vector<vector<T>> name(n, vector<type>(m)); in(name)
-#define fin(...) {out(__VA_ARGS__);return;}
 #define overload4(a, b, c, d, e, ...) e
 #define overload3(a, b, c, d, ...) d
 #define rep1(a) for(ll i=0;i<(ll)(a);i++)
@@ -23,7 +15,15 @@
 #define all3(i, a, b) begin(i)+a,begin(i)+b
 #define all(...) overload3(__VA_ARGS__,all3,all2,all1)(__VA_ARGS__)
 #define rall(n) (n).rbegin(),(n).rend()
-#define pb push_back
-#define eb emplace_back
 #define a first
 #define b second
+#define pb push_back
+#define eb emplace_back
+#define I(...) int __VA_ARGS__;in(__VA_ARGS__)
+#define L(...) long long __VA_ARGS__;in(__VA_ARGS__)
+#define S(...) string __VA_ARGS__;in(__VA_ARGS__)
+#define C(...) char __VA_ARGS__;in(__VA_ARGS__)
+#define D(...) double __VA_ARGS__;in(__VA_ARGS__)
+#define V(T, name, n) vector<T> name(n); in(name)
+#define VV(T, name, n, m) vector<vector<T>> name(n, vector<type>(m)); in(name)
+#define fin(...) {out(__VA_ARGS__);return;}
