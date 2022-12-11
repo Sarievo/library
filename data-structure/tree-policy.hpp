386383350a6f12c38f6fe51a4dd23df7b3283438
sarievo <sarievo@protonmail.com>
2022-12-11 18:01:40 +0800

    Changed styling

--- data-structure/tree-policy.hpp
+++ data-structure/tree-policy.hpp
@@ -5,9 +5,7 @@ using namespace __gnu_pbds;
 // find_by_order() - returns an iterator to the kth-largest element (counting from zero)
 // order_of_key()  - the number of items in a set that are strictly smaller than our item
 template<class T> using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template<class T> using indexed_multiset = tree<T, null_type, less_equal<T>,
-rb_tree_tag,
-tree_order_statistics_node_update>;
+template<class T> using indexed_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 /**
 * @brief Tree Policy
 */
